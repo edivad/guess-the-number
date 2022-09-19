1f8e1d0a5d4e8dce63b3a7e73330c52e8a5a60e5
Davide Pavan <davide.pavan@bitrock.it>
2022-09-19 17:57:37 +0200

    Program to F[_]

--- scala/src/main/scala/com/example/guessthenumber/programs/Program.scala
+++ scala/src/main/scala/com/example/guessthenumber/programs/Program.scala
@@ -1,11 +1,14 @@
 package com.example.guessthenumber.programs
 
-import cats.Id
+import cats.implicits.{toFlatMapOps, toFunctorOps}
+import cats.instances.{int, string}
+import cats.syntax.{flatMap, functor}
+import cats.{Applicative, Id, Monad}
 import com.example.guessthenumber.services.{Console, Random}
 
 import scala.annotation.tailrec
 
-class Program(random: Random[Id], console: Console[Id]) {
+class Program[F[_]: Monad](random: Random[F], console: Console[F]) {
 
   sealed trait Result
   case object InvalidInput extends Result
@@ -13,32 +16,40 @@ class Program(random: Random[Id], console: Console[Id]) {
   case object TooHigh      extends Result
   case object TooLow       extends Result
 
-  def run(): Unit = {
-    loop(random.nextInt(100))
-  }
+  def run(): F[Unit] = for {
+    toBeGuessed <- random.nextInt(100)
+    _           <- loop(toBeGuessed)
+  } yield ()
 
-  @tailrec
-  private def loop(toBeGuessed: Int): Unit = {
-    val input  = console.readLine()
-    val result = check(toBeGuessed, input)
-    console.printLine(messageFor(result))
-    result match
-      case Guessed => ()
+  private def loop(toBeGuessed: Int): F[Unit] = for {
+    input   <- console.readLine()
+    result  <- check(toBeGuessed, input)
+    message <- messageFor(result)
+    _       <- console.printLine(message)
+    _ <- result match {
+      case Guessed => exit
       case _       => loop(toBeGuessed)
-  }
+    }
+  } yield ()
 
-  private def check(toBeGuessed: Int, input: String) =
-    input.toIntOption
+  private val exit = Applicative[F].pure(())
+
+  private def check(toBeGuessed: Int, input: String): F[Result] = {
+    val result = input.toIntOption
       .fold(InvalidInput) { number =>
         if (number == toBeGuessed) Guessed
         else if (number > toBeGuessed) TooHigh
         else TooLow
       }
+    Applicative[F].pure(result)
+  }
 
-  private def messageFor(result: Result) =
-    result match
+  private def messageFor(result: Result): F[String] = {
+    val str = result match
       case InvalidInput => "not a number"
       case Guessed      => "Guessed"
       case TooHigh      => "Too high"
       case TooLow       => "Too low"
+    Applicative[F].pure(str)
+  }
 }

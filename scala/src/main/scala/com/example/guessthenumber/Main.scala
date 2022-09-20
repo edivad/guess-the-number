e9b3342930b020bfe6b2904d369f71dc793623e5
Davide Pavan <davide.pavan@bitrock.it>
2022-09-20 09:22:29 +0200

    Used Cats' Console

--- scala/src/main/scala/com/example/guessthenumber/Main.scala
+++ scala/src/main/scala/com/example/guessthenumber/Main.scala
@@ -1,6 +1,7 @@
 package com.example.guessthenumber
 
 import cats.Id
+import cats.effect.std.Console as CatsConsole
 import cats.effect.{IO, IOApp}
 import com.example.guessthenumber.programs.Program
 import com.example.guessthenumber.services.{Console, Random}
@@ -13,8 +14,10 @@ object Main extends IOApp.Simple {
   private val random = Random.make[IO]
 
   private val console = new Console[IO] {
-    def printLine(s: String): IO[Unit] = IO.println(s)
-    def readLine(): IO[String]         = IO.readLine
+    private val catsConsole = CatsConsole[IO]
+
+    def printLine(s: String): IO[Unit] = catsConsole.println(s)
+    def readLine(): IO[String]         = catsConsole.readLine
   }
 
   private val program = new Program(random, console)
